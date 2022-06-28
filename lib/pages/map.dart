091e51d94452f1e8324c83c6d257783d22d39ec7
Hugo Piedanna <h.piedanna@gmail.com>
2022-06-28 21:24:39 +0200

    Adding map view

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
-class map extends StatelessWidget {
+class map extends StatefulWidget {
+  const map({Key? key}) : super(key: key);
+
+  @override
+  State<map> createState() => _mapView();
+}
+
+class _mapView extends State<map> with AutomaticKeepAliveClientMixin<map> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -12,9 +21,34 @@ class map extends StatelessWidget {
       child: Scaffold(
         backgroundColor: Colors.transparent,
         body: Center(
-          child: Text('Carte'),
+          child: Flexible(
+            child: FlutterMap(
+              options: MapOptions(
+                center: LatLng(51.5, -0.09),
+                zoom: 15.0,
+              ),
+              layers: [
+                TileLayerOptions(
+                  urlTemplate:
+                      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  subdomains: ['a', 'b', 'c'],
+                  tileProvider: const NonCachingNetworkTileProvider(),
+                ),
+              ],
+              nonRotatedChildren: [
+                AttributionWidget.defaultWidget(
+                  source: 'OpenStreetMap contributors',
+                  onSourceTapped: () {},
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
   }
+
+  @override
+  // TODO: implement wantKeepAlive
+  bool get wantKeepAlive => true;
 }
