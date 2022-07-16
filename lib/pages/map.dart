bf56113d69c83abf3400c2ab05cc9a297d114ac2
Hugo Piedanna <h.piedanna@gmail.com>
2022-07-16 22:39:48 +0200

    Adding secure system for map page

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -1,5 +1,9 @@
+import 'package:chasse_infos/index.dart';
+import 'package:chasse_infos/pages/profil.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:hexcolor/hexcolor.dart';
 import 'package:latlong2/latlong.dart';
 
 class map extends StatefulWidget {
@@ -10,6 +14,8 @@ class map extends StatefulWidget {
 }
 
 class _mapView extends State<map> with AutomaticKeepAliveClientMixin<map> {
+  User? user = FirebaseAuth.instance.currentUser;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -21,27 +27,69 @@ class _mapView extends State<map> with AutomaticKeepAliveClientMixin<map> {
       child: Scaffold(
         backgroundColor: Colors.transparent,
         body: Center(
-          child: Flexible(
-            child: FlutterMap(
-              options: MapOptions(
-                center: LatLng(43.40676081436447, 4.988552087050192),
-                zoom: 12.0,
-              ),
-              layers: [
-                TileLayerOptions(
-                  urlTemplate:
-                      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                  subdomains: ['a', 'b', 'c'],
-                  tileProvider: const NonCachingNetworkTileProvider(),
-                ),
-              ],
-            ),
-          ),
-        ),
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            (user == null)
+                ? _showNoLog(context)
+                : Flexible(
+                    child: FlutterMap(
+                      options: MapOptions(
+                        center: LatLng(43.40676081436447, 4.988552087050192),
+                        zoom: 12.0,
+                      ),
+                      layers: [
+                        TileLayerOptions(
+                          urlTemplate:
+                              'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                          subdomains: ['a', 'b', 'c'],
+                          tileProvider: const NonCachingNetworkTileProvider(),
+                        ),
+                      ],
+                    ),
+                  ),
+          ],
+        )),
       ),
     );
   }
 
   @override
   bool get wantKeepAlive => true;
+
+  Widget _showNoLog(BuildContext context) {
+    return new AlertDialog(
+      backgroundColor: Color(0xff8fc0a9),
+      title: const Text("Vous n'êtes pas connecté",
+          style: TextStyle(
+              color: Color(0xfffaf3dd),
+              fontWeight: FontWeight.bold,
+              fontSize: 22)),
+      content: new Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const <Widget>[
+          Text(
+            "Pour utilisé cette fonctionnalité vous devez vous connecter. Pour profiter au maximum de cette fonctionnalité vous devez vous abonner à 1 ou plusieurs point de chasse",
+            style: TextStyle(color: Color(0xfffaf3dd), fontSize: 17),
+          ),
+        ],
+      ),
+      actions: <Widget>[
+        new FlatButton(
+          onPressed: () {
+            Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(builder: (context) => profil()),
+                (route) => false);
+          },
+          textColor: Theme.of(context).primaryColor,
+          child: Text(
+            "Compris",
+            style: TextStyle(color: HexColor('#C8D5B9')),
+          ),
+        ),
+      ],
+    );
+  }
 }
