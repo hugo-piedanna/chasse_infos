510273a565facf15f5d4a7ea2740dac19913532a
Hugo Piedanna <h.piedanna@gmail.com>
2022-07-19 17:24:59 +0200

    Adding subcribe check for map page

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -1,5 +1,7 @@
 import 'package:chasse_infos/index.dart';
+import 'package:chasse_infos/models/userModel.dart';
 import 'package:chasse_infos/pages/profil.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
@@ -13,8 +15,32 @@ class map extends StatefulWidget {
   State<map> createState() => _mapView();
 }
 
-class _mapView extends State<map> with AutomaticKeepAliveClientMixin<map> {
+class _mapView extends State<map> {
   User? user = FirebaseAuth.instance.currentUser;
+  userModel userMod = userModel();
+  bool checkHunt = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserData();
+  }
+
+  void _loadUserData() async {
+    if (user != null) {
+      await FirebaseFirestore.instance
+          .collection("users")
+          .doc(user!.uid)
+          .get()
+          .then((value) {
+        userMod = userModel.fromMap(value.data());
+        List? list = userMod.huntPoints;
+        setState(() {
+          checkHunt = list!.isEmpty;
+        });
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,59 +58,69 @@ class _mapView extends State<map> with AutomaticKeepAliveClientMixin<map> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             (user == null)
-                ? _showNoLog(context)
-                : Flexible(
-                    child: FlutterMap(
-                      options: MapOptions(
-                        center: LatLng(43.40676081436447, 4.988552087050192),
-                        zoom: 12.0,
-                      ),
-                      layers: [
-                        TileLayerOptions(
-                          urlTemplate:
-                              'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                          subdomains: ['a', 'b', 'c'],
-                          tileProvider: const NonCachingNetworkTileProvider(),
+                ? _showNoLog(context, "Vous n'êtes pas connecté",
+                    "Pour utilisé cette fonctionnalité vous devez vous connecter. Pour profiter au maximum de cette fonctionnalité vous devez vous abonner à 1 ou plusieurs point de chasse")
+                : (checkHunt)
+                    ? _showNoLog(context, "Abonnement",
+                        "Vous êtes abonné à aucun point de chasse, vous ne verrez donc aucun repère sur la carte")
+                    : Flexible(
+                        child: FlutterMap(
+                          options: MapOptions(
+                            center:
+                                LatLng(43.40676081436447, 4.988552087050192),
+                            zoom: 12.0,
+                          ),
+                          layers: [
+                            TileLayerOptions(
+                              urlTemplate:
+                                  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                              subdomains: ['a', 'b', 'c'],
+                              tileProvider:
+                                  const NonCachingNetworkTileProvider(),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                  ),
+                      ),
           ],
         )),
       ),
     );
   }
 
-  @override
-  bool get wantKeepAlive => true;
-
-  Widget _showNoLog(BuildContext context) {
-    return new AlertDialog(
+  Widget _showNoLog(BuildContext context, String title, String lore) {
+    return AlertDialog(
       backgroundColor: HexColor('#68B0AB'),
-      title: const Text("Vous n'êtes pas connecté",
-          style: TextStyle(
+      title: Text(title,
+          style: const TextStyle(
               color: Color(0xfffaf3dd),
               fontWeight: FontWeight.bold,
               fontSize: 22)),
-      content: new Column(
+      content: Column(
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: const <Widget>[
+        children: <Widget>[
           Text(
-            "Pour utilisé cette fonctionnalité vous devez vous connecter. Pour profiter au maximum de cette fonctionnalité vous devez vous abonner à 1 ou plusieurs point de chasse",
-            style: TextStyle(color: Color(0xfffaf3dd), fontSize: 17),
+            lore,
+            style: const TextStyle(color: Color(0xfffaf3dd), fontSize: 17),
           ),
         ],
       ),
       actions: <Widget>[
-        new FlatButton(
+        FlatButton(
           onPressed: () {
-            Navigator.of(context).pushAndRemoveUntil(
-                MaterialPageRoute(builder: (context) => profil()),
-                (route) => false);
+            if (user == null) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const profil()),
+              );
+            } else if (checkHunt) {
+              setState(() {
+                checkHunt = false;
+              });
+            }
           },
           textColor: Theme.of(context).primaryColor,
-          child: Text(
+          child: const Text(
             "Compris",
             style: TextStyle(color: Color(0xffC8D5B9)),
           ),
